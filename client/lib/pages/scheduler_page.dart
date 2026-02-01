75a4881cf540dbbf75e1a9e672d99d731810d964
Ziga Koracin <ziga.koracin@gmail.com>
2026-02-01 19:46:08 +0100

    feat: tabs with nav to settings, status and scheduler page

---
+++ client/lib/pages/scheduler_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SchedulerPage extends StatelessWidget {
+  const SchedulerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Scheduler', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
