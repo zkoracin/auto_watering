577fabb1974beadb0e049e55d351c7eb311aaa5c
Ziga Koracin <ziga.koracin@gmail.com>
2026-02-02 11:32:33 +0100

    feat: user can test esp connection

---
+++ client/lib/models/esp_connection.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+enum EspConnectionState { idle, testing, success, failure }
+
+extension EspConnectionStateExtension on EspConnectionState {
+  String get buttonText {
+    switch (this) {
+      case EspConnectionState.testing:
+        return 'Testing connection...';
+      case EspConnectionState.success:
+        return 'ESP Connected';
+      case EspConnectionState.failure:
+        return 'Connection Failed';
+      case EspConnectionState.idle:
+        return 'ESP Connection';
+    }
+  }
+  
+  Icon icon(ColorScheme colors) {
+    switch (this) {
+      case EspConnectionState.success:
+        return Icon(Icons.check_circle, color: colors.primary);
+      case EspConnectionState.failure:
+        return Icon(Icons.error, color: colors.error);
+      default:
+        return Icon(Icons.wifi, color: colors.onSurface);
+    }
+  }
+}
